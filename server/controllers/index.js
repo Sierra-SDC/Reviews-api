e506d31184b320d81889412f6bec5694a7483a39
Andrew Huang <ahuang0225@gmail.com>
2021-06-25 19:43:08 -0400

    Implemented getReviews

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,24 +7,65 @@ const CharacteristicReviews = require('../models/characteristicReviews');
 
 module.exports = {
   getReviews: (req, res) => {
+    const page = req.query.page || 0;
+    const count = req.query.count || 5;
     Reviews.findAll({
       where: {
-        product_id: {
-          [Op.eq]: req.query.product_id,
-        },
+        product_id: req.query.product_id,
+        reported: false,
       },
-      // include: [
-      //   {
-      //     model: Photos,
-      //     attributes: { exclude: '$review_id$' },
-      //   },
-      // ],
+      attributes: {
+        exclude: ['product_id', 'reported', 'reviewer_email'],
+      },
+      limit: count,
+      include: [
+        {
+          model: Photos,
+          attributes: {
+            exclude: ['review_id'],
+          },
+        },
+      ],
     })
-      .then((result) => {
-        console.log('@@@@@', result);
+      .then((data) => {
+        const reviews = data.map((review) => {
+          let {
+            review_id,
+            rating,
+            date,
+            summary,
+            body,
+            recommend,
+            reviewer_name,
+            response,
+            helpfulness,
+            photos 
+          } = review;
+
+          let newDate = new Date(Number(date));
+
+          return {
+            review_id,
+            rating,
+            summary,
+            recommend,
+            response,
+            body,
+            date: newDate,
+            reviewer_name,
+            helpfulness,
+            photos,
+          };
+        });
+        const result = {
+          product: req.query.product_id,
+          page,
+          count: reviews.length,
+          results: reviews,
+        };
         res.status(200).json(result);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => res.send(err));
   },
   helpfulReview: (req, res) => {
     Reviews.update(
