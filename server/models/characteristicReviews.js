4322eeb7446a1c0bd646091455a0228f0fce9765
Andrew Huang <ahuang0225@gmail.com>
2021-06-26 18:14:30 -0400

    Implemented Index

--- server/models/characteristicReviews.js
+++ server/models/characteristicReviews.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const sequelize = require('../../util/database');
 const db = require('../../util/database');
 
 const CharacteristicReviews = db.define('characteristic_reviews', {
@@ -20,6 +21,18 @@ const CharacteristicReviews = db.define('characteristic_reviews', {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
+},
+{
+  sequelize,
+  modelName: 'characteristic_reviews',
+  indexes: [
+    {
+      unique: false,
+      fields: ['characteristic_id', 'review_id'],
+    },
+  ],
 });
+
+// CharacteristicReviews.sync({ alter: true });
 
 module.exports = CharacteristicReviews;
