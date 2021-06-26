aae71fdb56efef1ca888d3e02cb87ebadef12687
Andrew Huang <ahuang0225@gmail.com>
2021-06-26 18:12:01 -0400

    Removed unnecessary tables

--- schema.sql
+++ schema.sql
@@ -1,41 +1,14 @@
-CREATE TABLE reviews (
-    review_id INT PRIMARY KEY, 
-    product_id INT, 
-    rating INT, 
-    date BIGINT, 
-    summary VARCHAR (200), 
-    body VARCHAR (500), 
-    recommend BOOLEAN, 
-    reported BOOLEAN, 
-    reviewer_name TEXT, 
-    reviewer_email TEXT,    
-    response TEXT, 
-    helpfulness INT
-);
+-- products DATA
+-- COPY products FROM '/Users/andrewhuang/Downloads/product.csv' DELIMITER ',' CSV HEADER;
 
+--reviews DATA
 -- COPY reviews FROM '/Users/andrewhuang/Downloads/reviews.csv' DELIMITER ',' CSV HEADER;
 
-CREATE TABLE photos (
-    id INT PRIMARY KEY,
-    review_id INT,
-    url VARCHAR (300)
-);
-
+--photos DATA
 -- COPY photos FROM '/Users/andrewhuang/Downloads/reviews_photos.csv' DELIMITER ',' CSV HEADER;
 
-CREATE TABLE characteristics (
-    id INT PRIMARY KEY,
-    product_id INT,
-    name TEXT
-);
-
+-- characteristics DATA
 -- COPY characteristics FROM '/Users/andrewhuang/Downloads/characteristics.csv' DELIMITER ',' CSV HEADER;
 
-CREATE TABLE characteristic_reviews (
-    id INT PRIMARY KEY,
-    characteristic_id INT,
-    review_id INT,
-    value INT
-);
-
+-- characteristic_reviews DATA
 -- COPY characteristic_reviews FROM '/Users/andrewhuang/Downloads/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
