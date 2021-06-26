5b1cc4d50c199470b107034dd48e716b793981ed
Andrew Huang <ahuang0225@gmail.com>
2021-06-26 18:14:05 -0400

    Incorporated Model Relationships/Included Index

--- server/models/characteristics.js
+++ server/models/characteristics.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
+const sequelize = require('../../util/database');
 const db = require('../../util/database');
+const CharacteristicReviews = require('./characteristicReviews');
 
 const Characteristics = db.define('characteristics', {
   id: {
@@ -16,6 +18,28 @@ const Characteristics = db.define('characteristics', {
     type: Sequelize.STRING,
     allowNull: false,
   },
+},
+{
+  sequelize,
+  modelName: 'characteristics',
+  indexes: [
+    {
+      unique: false,
+      fields: ['product_id'],
+    },
+  ],
 });
+
+Characteristics.hasMany(CharacteristicReviews, {
+  foreignKey: 'characteristic_id',
+  constraints: false,
+});
+
+CharacteristicReviews.belongsTo(Characteristics, {
+  foreignKey: 'characteristic_id',
+  constraints: false,
+});
+
+// Characteristics.sync({ alter: true });
 
 module.exports = Characteristics;
