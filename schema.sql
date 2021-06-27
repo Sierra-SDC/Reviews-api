51e0ebf49415735daf1ee1acc3e31b1c4a471b35
Andrew Huang <ahuang0225@gmail.com>
2021-06-27 16:04:15 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -12,3 +12,15 @@
 
 -- characteristic_reviews DATA
 -- COPY characteristic_reviews FROM '/Users/andrewhuang/Downloads/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+
+-- SELECT setval('products_product_id_seq', (SELECT count(*) FROM products), true);
+-- SELECT setval('reviews_review_id_seq', (SELECT count(*) FROM reviews), true);
+-- SELECT setval('photos_id_seq', (SELECT count(*) FROM photos), true);
+-- SELECT setval('characteristics_id_seq', (SELECT count(*) FROM characteristics), true);
+-- SELECT setval('characteristic_reviews_id_seq', (SELECT count(*) FROM characteristic_reviews), true);
+
+-- SELECT setval('serial', max(product_id)) FROM products;
+-- SELECT setval('serial', max(review_id)) FROM reviews;
+-- SELECT setval('serial', max(id)) FROM photos;
+-- SELECT setval('serial', max(id)) FROM characteristics;
+-- SELECT setval('serial', max(id)) FROM characteristic_reviews;
